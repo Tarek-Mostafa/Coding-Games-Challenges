1811227af9b18639c8d30deca6e2db118c8659a8
Tarek Mostafa <Tarek-Mostafa@users.noreply.github.com>
2017-09-02 01:22:19 +0200

    Update Temperatures.js

--- Easy/Temperatures.js
+++ Easy/Temperatures.js
@@ -7,16 +7,32 @@ var n = parseInt(readline()); // the number of temperatures to analyse
 var temps = readline(); // the n temperatures expressed as integers ranging from -273 to 5526
 var tempsArr = temps.split(" ");
 
+function testCases (arr) {
+    if(!temps) {
+        var min = 0;
+    }
+    
+    if(arr.length == 1) {
+        var min = arr[0] || 0;
+    }
+
+    else {
+        return nearestZero(arr);
+    }
+
+    return min;
+}
+
 function nearestZero(arr) {
 
     var min;    // Number to be returned
-
+    
     arr.sort(function (a, b) {
         return b - a;
     });
 
     for(var i = 0, n = arr.length; i < n; i++) {
-    
+
         var firstItem = parseInt(arr[0]);
         var currentI = parseInt(arr[i]);
         
@@ -40,15 +56,11 @@ function nearestZero(arr) {
         }
     }
 
-    if(!temps) {
-        min = 0;
-    }
-
     return min;
 }
 
 
 // Write an action using print()
 // To debug: printErr('Debug messages...');
-var tempreture = nearestZero(tempsArr);
+var tempreture = testCases(tempsArr);
 print(tempreture);
